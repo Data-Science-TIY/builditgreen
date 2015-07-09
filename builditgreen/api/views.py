341cc073315dccf91c03c20222bc8937abc9cba3
Josh Higgins <jshhggns@gmail.com>
2015-07-09 11:42:03 -0400

    Added trend API views

--- builditgreen/api/views.py
+++ builditgreen/api/views.py
@@ -12,15 +12,40 @@ class StateMapListView(generics.ListAPIView):
     queryset = State.objects.all()
 
 
-class TrendsOverview(APIView):
+class AllTrends(APIView):
 
     def get(self, request, format=None):
         """
         Return trends by year.
         """
-        year_2013 = {2013: Project.objects.filter(certification_date__gte=datetime(2013, 1, 30))\
-            .filter(certification_date__lte=datetime(2013, 12, 30)).count()}
-        return Response(year_2013)
+        trends_dict = {}
+        year_list = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014]
+        projects_certified_dict = {i: Project.objects.filter
+            (certification_date__gte=datetime(i, 1, 1)).filter(certification_date__lte=datetime(i, 12, 31))
+            .count() for i in year_list}
+
+        trends_dict["total_certifications"] = projects_certified_dict
+
+
+        # projects_certified_2014 = {"Total Projects Certified 2014": Project.objects.filter
+        #     (certification_date__gte=datetime(2014, 1, 1)).filter(certification_date__lte=datetime(2014, 12, 31))
+        #     .count()}
+        #
+        # projects_certified_2013 = {"Total Projects Certified 2013": Project.objects.filter
+        #     (certification_date__gte=datetime(2013, 1, 1)).filter(certification_date__lte=datetime(2013, 12, 31))
+        #     .count()}
+        #
+        # projects_certified_2012 = {"Total Projects Certified 2012": Project.objects.filter
+        #     (certification_date__gte=datetime(2012, 1, 1)).filter(certification_date__lte=datetime(2012, 12, 31))
+        #     .count()}
+        #
+        # projects_certified_2011 = {"Total Projects Certified 2011": Project.objects.filter
+        #     (certification_date__gte=datetime(2011, 1, 1)).filter(certification_date__lte=datetime(2011, 12, 31))
+        #     .count()}
+
+        return Response(trends_dict)
+
+
 
 
 
