b50484ca990c8d20f9943d89630fabdd6da485c1
Tripp Shealy <tripp.shealy@gmail.com>
2015-07-09 13:56:23 -0400

    scraper with sqlite database

--- builditgreen/scrape_script.py
+++ builditgreen/scrape_script.py
@@ -5,15 +5,16 @@ import urllib
 import re
 import pickle
 import csv
+import json
 
 import sqlite3
 
 def setup_db():
-    conn = sqlite3.connect('db.sqlite')
+    conn = sqlite3.connect('scrape.sqlite3')
     c = conn.cursor()
     # c.execute("""CREATE TABLE "api_project" ("id" integer NOT NULL PRIMARY KEY, "is_confidential" varchar(255) NOT NULL, "name" varchar(255) NULL, "street" varchar(255) NULL, "city" varchar(255) NULL, "zip_code" varchar(255) NULL, "country" varchar(255) NULL, "leed_version" varchar(255) NULL, "points_achieved" integer NULL, "certification_level" varchar(255) NULL, "certification_date" date NULL, "owner_types" varchar(255) NULL, "gross_square_foot" bigint NULL, "total_property_area" bigint NULL, "project_types" varchar(255) NULL, "registration_date" date NULL);
     # """)
-    c.execute('CREATE TABLE "points_data" ("id" integer NOT NULL PRIMARY KEY, "points_achieved")')
+    c.execute('CREATE TABLE "points_data" ("id" integer NOT NULL PRIMARY KEY, "points_achieved" text)')
     conn.commit()
     conn.close()
 
@@ -33,15 +34,23 @@ def pull_score_card(id):
     return scores_dict
 
 def scraper():
-    with open('public_project_ids.csv', encoding='ISO-8859-1') as infile:
+    with open('public_project_ids1.csv', encoding='ISO-8859-1') as infile:
         ids = csv.reader(infile)
-        scores = []
         for id in ids:
             print(id)
             one_score = pull_score_card(id[0])
-            scores.append(one_score)
-    with open('public_scores.pickle', 'wb') as handle:
-        pickle.dump(scores, handle)
+            one_score_key, one_score_value = list(one_score.items())[0]
+            conn = sqlite3.connect('scrape.sqlite3')
+            c = conn.cursor()
+            c.execute("INSERT OR IGNORE INTO points_data (id, points_achieved) VALUES (?, ?)",
+                      (one_score_key, json.dumps(one_score_value)))
+
+            conn.commit()
+            conn.close()
 
 
 
+
+if __name__ == "__main__":
+    setup_db()
+    scraper()
