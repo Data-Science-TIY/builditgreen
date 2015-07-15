846c02928044450cfa1120e8e1acbeeaccfccb00
Mark Harper <markharper20@gmail.com>
2015-07-15 18:38:44 -0400

    working on big graph

--- src/js/datavis/gradients.js
+++ src/js/datavis/gradients.js
@@ -1,13 +1,18 @@
 var buildgradient = require('./buildgradient');
 
 var numberCertified = buildgradient('#FFCDD2', '#B71C1C');
-var numberProjects = buildgradient('#C8E6C9','#1B5E20');
-var popCorrect = buildgradient('#FFF9C4','#F57F17');
-var gold = buildgradient('#FFCCBC','#BF360C');
-var silver = buildgradient('#E1BEE7','#4A148C');
-var platinum = buildgradient('#B3E5FC','#01579B');
+var numberProjects = buildgradient('#C8E6C9', '#1B5E20');
+var popCorrect = buildgradient('#FFF9C4', '#F57F17');
+var gold = buildgradient('#FFCCBC', '#BF360C');
+var silver = buildgradient('#E1BEE7', '#4A148C');
+var platinum = buildgradient('#B3E5FC', '#01579B');
 //console.log(numberCertified);
 
-module.exports = {"number_of_projects":numberProjects,"number_certified": numberCertified
-	, "number_of_projects_population_corrected": popCorrect, 'number_gold': gold, "number_silver": silver,
-	"number_platinum": platinum};
+module.exports = {
+    "number_of_projects": numberProjects,
+    "number_certified": numberCertified,
+    "number_of_projects_population_corrected": popCorrect,
+    'number_gold': gold,
+    "number_silver": silver,
+    "number_platinum": platinum
+};
