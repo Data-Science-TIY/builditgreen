36406282056c09523a9511e00fc6384f85da71bc
Josh Higgins <jshhggns@gmail.com>
2015-07-21 11:46:55 -0400

    setup memcachier

--- builditgreen/builditgreen/heroku_settings.py
+++ builditgreen/builditgreen/heroku_settings.py
@@ -25,13 +25,43 @@ STATIC_ROOT = 'staticfiles'
 import os
 
 CACHES = {
+
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
-        'USERNAME': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
-        'PASSWORD': os.environ.get('MEMCACHEDCLOUD_PASSWORD'),
+        # Use pylibmc
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+
+        # Use binary memcache protocol (needed for authentication)
+        'BINARY': True,
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+        'LOCATION': os.environ.get('MMEMCACHIER_SERVERS').split(','),
+        'USERNAME': os.environ.get('MEMCACHIER_USERNAME'),
+        'PASSWORD': os.environ.get('MEMCACHIER_PASSWORD'),
+
+        'OPTIONS': {
+            # Enable faster IO
+            'no_block': True,
+            'tcp_nodelay': True,
+
+            # Keep connection alive
+            'tcp_keepalive': True,
+
+            # Timeout for set/get requests
+            '_poll_timeout': 2000,
+
+            # Use consistent hashing for failover
+            'ketama': True,
+
+            # Configure failover timings
+            'connect_timeout': 2000,
+            'remove_failed': 4,
+            'retry_timeout': 2,
+            'dead_timeout': 10
         }
-    }
+    }}
 
 LOGGING = {
     "version": 1,
